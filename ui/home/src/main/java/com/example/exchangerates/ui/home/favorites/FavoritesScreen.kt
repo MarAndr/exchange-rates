6bef1787d35b27d279c7131aa0959d163bce0bef
Andrey Marin <a.marin@modulbank.ru>
2025-06-29 12:18:18 +0200

    fix wrong padding from bottom nav

--- ui/home/src/main/java/com/example/exchangerates/ui/home/favorites/FavoritesScreen.kt
+++ ui/home/src/main/java/com/example/exchangerates/ui/home/favorites/FavoritesScreen.kt
@@ -7,9 +7,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.exclude
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -73,6 +78,8 @@ private fun FavoritesScreen(
             )
         },
         containerColor = AppTheme.color.bg.default,
+        contentWindowInsets = WindowInsets.safeDrawing
+            .exclude(WindowInsets.safeDrawing.only(WindowInsetsSides.Bottom)),
     ) { paddingValues ->
         val refreshState = rememberPullToRefreshState()
         val isRefreshing = screenState is FavoritesScreenState.Data && screenState.isRefreshing
