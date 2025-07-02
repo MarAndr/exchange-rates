efba92232d1a3d5d9cbc68da9136b7dcc53200aa
Andrey Marin <a.marin@modulbank.ru>
2025-07-02 16:23:59 +0200

    changed apikey

--- core/remote/src/main/java/com/example/exchangerates/core/remote/Constants.kt
+++ core/remote/src/main/java/com/example/exchangerates/core/remote/Constants.kt
@@ -1,5 +1,5 @@
 package com.example.exchangerates.core.remote
 
 object Constants {
-    const val API_KEY = "hOObkY6HSkGIeYVFsEvsaP0DIZhMYt2k"
+    const val API_KEY = "MtZJNwhZUuGS0PVXttjIjcLcK38JjIIA"
 }
